ae8ca11c49aa082f0ec28dccfaf48dcd5836f9af
Katrina Hirsch and Sheena Nickerson <katrina+sheena@>
2016-11-02 11:48:43 -0700

    implement shopping cart service

--- app/routes/book.js
+++ app/routes/book.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  shoppingCart: Ember.inject.service(),
   model(params) {
     return this.store.findRecord('book', params.book_id);
+  },
+  actions: {
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
+    }
   }
 });
