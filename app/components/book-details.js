ca79e89620fe9ed20492bbb0ab1d6367546d42b7
Katrina Hirsch and Sheena Nickerson <katrina+sheena@>
2016-11-03 16:21:32 -0700

    fix breadcrumb display

--- app/components/book-details.js
+++ app/components/book-details.js
@@ -5,5 +5,14 @@ export default Ember.Component.extend({
     addToCart(item) {
       this.sendAction('addToCart', item);
     }
-  }
+  },
+  genre: Ember.computed('book.genre.name', function() {
+    var genre = this.get('book.genre.name');
+    var output;
+    if(genre) {
+      output = genre.charAt(0).toUpperCase() + genre.slice(1);
+    }
+
+    return output;
+  })
 });
